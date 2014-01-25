01e8965ee8eca3f34880fab981c8767e692cc5ea
ueda <ueda@jsk.t.u-tokyo.ac.jp>
2014-01-25 10:46:33 +0000

    adding scripts to configure midi device interactively

---
+++ jsk_teleop_joy/scripts/interactive_midi_config.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+
+import pygame
+import pygame.midi
+import select
+import sys
+import yaml
+
+G_DEVICE_INFO = {
+  "device_name": "",
+  "analogs": [],                             #[[major_id, minor_id], [major_id, minor_id], ...
+  "buttons": []
+  }
+
+class ParseException(Exception):
+  pass
+
+def parseDeviceName():
+  global G_DEVICE_INFO
+  devices = pygame.midi.get_count()
+  print "First, we choose device name:"
+  for d in range(devices):
+    info = pygame.midi.get_device_info(d)
+    if info[2] == 1:
+      print "  [%d] %s (%s)" % (d, info[1], "input")
+    else:
+      print "  [%d] %s (%s)" % (d, info[1], "output")
+  val = raw_input("Please select the device by number[%d-%d]:" % (0, d))
+  try:
+    parsed_number = int(val)
+    if parsed_number >= 0 and parsed_number <= d:
+      name = pygame.midi.get_device_info(parsed_number)[1]
+      G_DEVICE_INFO["device_name"] = name
+      print ""
+      print "device_name: %s"  % (name)
+      return parsed_number
+    else:
+      raise ParseException("please input number bewtween %d to %d" % (0, d))
+  except ValueError:
+    raise ParseException("please input number")
+
+def configAnalogInputs(controller):
+  global G_DEVICE_INFO
+  print "Please move analog inputs"
+  print "The order you move them will be mapped into Joy/axes."
+  print "If you want to finish analog mapping, please type 'q'"
+  analog_configs = []
+  while True:
+    ready = select.select([sys.stdin], [], [], 0.1)[0]
+    if ready:
+      line = sys.stdin.readline()
+      if line.startswith("q"):
+        print "We installed %d analog inputs" % (len(analog_configs))
+        G_DEVICE_INFO["analogs"] = analog_configs
+        return
+    while controller.poll():
+      data = controller.read(1)
+      for elem_set in data:
+        elem = elem_set[0]
+        major_id = elem[0]
+        minor_id = elem[1]
+        # check that is already installed or not
+        if (major_id, minor_id) not in analog_configs:
+          print "(%d, %d) installing into %d" % (major_id, minor_id, len(analog_configs))
+          analog_configs.append((major_id, minor_id))
+          
+def configButtonInputs(controller):
+  global G_DEVICE_INFO
+  print "Please push the buttons"
+  print "The order you move them will be mapped into Joy/buttons."
+  print "If you want to finish buttons mapping, please type 'q'"
+  button_configs = []
+  while True:
+    ready = select.select([sys.stdin], [], [], 0.1)[0]
+    if ready:
+      line = sys.stdin.readline()
+      if line.startswith("q"):
+        print "We installed %d button inputs" % (len(button_configs))
+        G_DEVICE_INFO["buttons"] = button_configs
+        return
+    while controller.poll():
+      data = controller.read(1)
+      for elem_set in data:
+        elem = elem_set[0]
+        major_id = elem[0]
+        minor_id = elem[1]
+        # check that is already installed or not
+        if major_id == 144:
+          if minor_id not in button_configs:
+            print "%dinstalling into %d" % (minor_id, len(button_configs))
+            button_configs.append(minor_id)
+  
+def main():
+  pygame.midi.init()
+  while True:
+    try:
+      device_num = parseDeviceName()
+      break
+    except ParseException, e:
+      print e.message
+      print ""
+      continue
+  controller = pygame.midi.Input(device_num)
+  configAnalogInputs(controller)
+  configButtonInputs(controller)
+  f = open('/tmp/midi.yaml', 'w')
+  f.write(yaml.dump(G_DEVICE_INFO))
+  f.close()
+  print "writing the configuration to /tmp/midi.yaml"
+  
+if __name__ == "__main__":
+  main()
+
+  
---
+++ jsk_teleop_joy/scripts/midi_config_player.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+import pygame
+import pygame.midi
+import select
+import sys
+import yaml
+import rospy
+import roslib
+
+from sensor_msgs.msg import Joy
+
+roslib.load_manifest('jsk_joy')
+
+def openMidiDeviceWithName(device_name):
+  devices = pygame.midi.get_count()
+  for i in range(devices):
+    info = pygame.midi.get_device_info(i)
+    if info[1] == device_name and info[2] == 1:
+      return pygame.midi.Input(i)
+  raise Exception("Cannot find the device: %s" % (device_name))
+    
+def main():
+  pygame.midi.init()
+  rospy.init_node('midi_joy')
+  # parse the arg
+  argv = rospy.myargv()
+  if len(argv) == 0:
+    rospy.logfatal("You need to specify config yaml file")
+    sys.exit(1)
+  config_file = argv[1]
+  joy_pub = rospy.Publisher("/joy", Joy)
+  with open(config_file, "r") as f:
+    config = yaml.load(f)
+    # open the device
+    controller = openMidiDeviceWithName(config["device_name"])
+    joy = Joy()
+    joy.axes = [0.0] * len(config["analogs"])
+    joy.buttons = [0] * len(config["buttons"])
+    while not rospy.is_shutdown():
+      joy.header.stamp = rospy.Time.now()
+      while controller.poll():
+        data = controller.read(1)
+        for elem_set in data:
+          elem = elem_set[0]
+          major_id = elem[0]
+          minor_id = elem[1]
+          value = elem[2]
+          if major_id == 144:              #button
+            if minor_id in config["buttons"]:
+              index = config["buttons"].index(minor_id)
+              if value == 127:
+                joy.buttons[index] = 1
+              elif value == 0:
+                joy.buttons[index] = 0
+          elif (major_id, minor_id) in config["analogs"]:
+            index = config["analogs"].index((major_id, minor_id))
+            joy.axes[index] = value / 127.0
+      joy_pub.publish(joy)
+      rospy.sleep(1.0 / 100.0)
+if __name__ == '__main__':
+  main()
+  
