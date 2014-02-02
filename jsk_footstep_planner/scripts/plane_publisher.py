c096e591f8cc1c56db979da2d944e6fd36fb2715
ueda <ueda@jsk.t.u-tokyo.ac.jp>
2014-02-02 04:59:19 +0000

    fix to keep orientation after projection to the planes

--- jsk_footstep_planner/scripts/plane_publisher.py
+++ jsk_footstep_planner/scripts/plane_publisher.py
@@ -9,18 +9,27 @@ from jsk_pcl_ros.msg import PolygonArray
 from geometry_msgs.msg import PolygonStamped, Point32
 
 def makePolygonArray():
-  global_frame_id = "/odom"
-  points_array = [[[1, 1, 0], [-1, 1, 0], [-1, -1, 0], [1, -1, 0]],
-                  [[1.5, 1, 0.2], [1, 1, 0.2], [1, -1, 0.2], [1.5, -1, 0.2]],
-                  [[2, 1, 0], [1.5, 1, 0], [1.5, -1, 0], [2, -1, 0]]]
+  global_frame_id = rospy.myargv()[1]
   ret = PolygonArray()
-  for points in points_array:
-    polygon = PolygonStamped()
-    polygon.header.frame_id = global_frame_id
-    polygon.header.stamp = rospy.Time.now()
-    for p in points:
-      polygon.polygon.points.append(Point32(*p))
-    ret.polygons.append(polygon)
+  ret.header.stamp = rospy.Time.now()
+  ret.header.frame_id = global_frame_id
+
+  for i in range(-1, 5):
+    # i = 0,  1, 2, 3, 4, 5
+    x1 = i * 3 - 0.5
+    x2 = i * 3 + 1 - 0.5
+    x3 = i * 3 + 2 - 0.5
+    x4 = i * 3 + 3 - 0.5
+    points_array = [[[x2, 2, 0],   [x1, 2, 0],   [x1, -2, 0],   [x2, -2, 0]],
+                    [[x3, 2, 0.2], [x2, 2, 0.2], [x2, -2, 0.2], [x3, -2, 0.2]],
+                    [[x4, 2, 0.1], [x3, 2, 0.1], [x3, -2, 0.1], [x4, -2, 0.1]]]
+    for points in points_array:
+      polygon = PolygonStamped()
+      polygon.header.frame_id = global_frame_id
+      polygon.header.stamp = rospy.Time.now()
+      for p in points:
+        polygon.polygon.points.append(Point32(*p))
+      ret.polygons.append(polygon)
   return ret
             
 
