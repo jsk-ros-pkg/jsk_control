2516d151cb206693c005c84079481e980989996f
ueda <ueda@jsk.t.u-tokyo.ac.jp>
2014-02-12 16:17:22 +0000

    catkinize jsk_ik_server

---
+++ jsk_ik_server/catkin.cmake
@@ -0,0 +1,14 @@
+cmake_minimum_required(VERSION 2.8.3)
+project(jsk_ik_server)
+
+catkin_package(
+    DEPENDS 
+    CATKIN-DEPENDS 
+    INCLUDE_DIRS 
+    LIBRARIES 
+)
+
+install(DIRECTORY euslisp test
+  DESTINATION ${CATKIN_PACKAGE_SHARE_DESTINATION}
+  USE_SOURCE_PERMISSIONS
+  )
