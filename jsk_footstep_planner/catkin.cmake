6db702fce72c731b08bc9f0874a71ec1f05216c0
ueda <ueda@jsk.t.u-tokyo.ac.jp>
2014-02-13 05:47:42 +0000

    fix dependency

--- jsk_footstep_planner/catkin.cmake
+++ jsk_footstep_planner/catkin.cmake
@@ -1,10 +1,10 @@
 cmake_minimum_required(VERSION 2.8.3)
 project(jsk_footstep_planner)
 
-find_package(catkin REQUIRED COMPONENTS jsk_footstep_msgs)
+find_package(catkin REQUIRED COMPONENTS jsk_footstep_msgs jsk_pcl_ros)
 catkin_package(
   DEPENDS
-  CATKIN-DEPENDS  jsk_footstep_msgs
+  CATKIN-DEPENDS  jsk_footstep_msgs jsk_pcl_ros
   INCLUDE_DIRS # TODO include
   LIBRARIES # TODO
   )
