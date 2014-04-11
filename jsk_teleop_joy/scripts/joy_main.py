9635dfc3fd9dbd37015fccf264266cacbfb28e0c
Ryohei Ueda <garaemon@gmail.com>
2014-04-11 22:26:58 +0900

    use load_manifest on groovy

--- jsk_teleop_joy/scripts/joy_main.py
+++ jsk_teleop_joy/scripts/joy_main.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+# only if groovy
+if os.environ["ROS_DISTRO"] == "groovy":
+  roslib.load_manifest('jsk_teleop_joy')
+
 import jsk_teleop_joy.joy
 
 if __name__ == '__main__':
