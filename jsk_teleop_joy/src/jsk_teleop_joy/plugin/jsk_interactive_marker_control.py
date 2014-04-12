80f77058b673eeb249a77e6c055bb58f24a22760
Masaki Murooka <murooka@jsk.t.u-tokyo.ac.jp>
2014-04-12 15:28:49 +0900

    rename plugin scripts to avoid msg import bug

--- jsk_teleop_joy/src/jsk_teleop_joy/plugin/jsk_interactive_marker_control.py
+++ jsk_teleop_joy/src/jsk_teleop_joy/plugin/jsk_interactive_marker_control.py
@@ -4,12 +4,18 @@ from joy_pose_6d import JoyPose6D
 from std_msgs.msg import String, Empty
 from geometry_msgs.msg import PoseStamped
 import xml.etree.ElementTree as ET
+from jsk_interactive_marker.msg import MarkerMenu
 
 class EndEffector(JoyPose6D):
   def __init__(self):
-    JoyPose6D.__init__(self, name='JoyGoPos')
+    JoyPose6D.__init__(self, name='EndEffector')
     self.support_follow_view = True
-    self.frame_id = rospy.get_param('~frame_id', '/base_link')
+    self.frame_id = rospy.get_param('~frame_id', 'base_link')
+    self.marker_menu_pub = rospy.Publisher('marker_menu', MarkerMenu)
   def joyCB(self, status, history):
+    if status.circle:
+      menu = MarkerMenu()
+      self.marker_menu_pub.publish(menu)
+      
     JoyPose6D.joyCB(self, status, history)
     
