04b66c331debf2970897a55a4958a5a0f92f074d
yusuke furuta <furuta@mensa.(none)>
2014-04-23 17:43:36 +0900

    add method to change move arm

--- jsk_teleop_joy/src/jsk_teleop_joy/plugin/jsk_interactive_marker_control.py
+++ jsk_teleop_joy/src/jsk_teleop_joy/plugin/jsk_interactive_marker_control.py
@@ -12,22 +12,31 @@ class EndEffector(JoyPose6D):
     self.support_follow_view = True
     self.frame_id = rospy.get_param('~frame_id', 'base_link')
     self.marker_menu_pub = rospy.Publisher('marker_menu', MarkerMenu)
+
+  def publishMarkerMenu(self, menu):
+    menu_msg = MarkerMenu()
+    menu_msg.menu = menu
+    self.marker_menu_pub.publish(menu_msg)
+
+
   def joyCB(self, status, history):
     if history.length() > 0:
       latest = history.latest()
     else:
-      latest = None
+      return
 
-    menu = MarkerMenu()
-    if status.circle and latest !=None and not latest.circle:
-      menu.menu = MarkerMenu.MOVE
-      self.marker_menu_pub.publish(menu)
+    #move
+    if status.circle and not latest.circle:
+      self.publishMarkerMenu(MarkerMenu.MOVE)
 
-    elif status.triangle and latest !=None and not latest.triangle:
-      if status.R1:
-        pass
-      elif status.L1:
-        pass
+    elif status.triangle:
+      #change move arm
+      if status.R1 and status.L1 and not (latest.R1 and latest.L1):
+        self.publishMarkerMenu(MarkerMenu.SET_MOVE_ARMS)
+      elif status.R1 and not latest.R1:
+        self.publishMarkerMenu(MarkerMenu.SET_MOVE_RARM)
+      elif status.L1 and not latest.L1:
+        self.publishMarkerMenu(MarkerMenu.SET_MOVE_LARM)
 
     else:
       JoyPose6D.joyCB(self, status, history)
