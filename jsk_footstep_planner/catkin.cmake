48b120c98488828ea41463d0e82eddb2fb459dac
Kei Okada <kei.okada@gmail.com>
2014-10-23 10:59:30 +0900

    roseus only needs runtime

--- jsk_footstep_planner/catkin.cmake
+++ jsk_footstep_planner/catkin.cmake
@@ -1,11 +1,11 @@
 cmake_minimum_required(VERSION 2.8.3)
 project(jsk_footstep_planner)
 
-find_package(catkin REQUIRED COMPONENTS jsk_footstep_msgs jsk_pcl_ros roseus)
+find_package(catkin REQUIRED COMPONENTS jsk_footstep_msgs jsk_pcl_ros)
 #catkin_python_setup()
 catkin_package(
   DEPENDS
-  CATKIN-DEPENDS  jsk_footstep_msgs jsk_pcl_ros roseus
+  CATKIN-DEPENDS  jsk_footstep_msgs jsk_pcl_ros
   INCLUDE_DIRS # TODO include
   LIBRARIES # TODO
   )
