bb6c892d66cb8b68409cb74069794889d8519ba7
Ryohei Ueda <garaemon@gmail.com>
2015-01-07 02:37:12 +0900

    [joy_mouse] Add name API to lookup device by name like "TPPS/2 IBM TrackPoint"

--- joy_mouse/scripts/disable_mouse.py
+++ joy_mouse/scripts/disable_mouse.py
@@ -7,7 +7,14 @@ try:
 except:
     import roslib; roslib.load_manifest("joy_mouse")
     import joy_mouse.xinput
+    
+import joy_mouse.joy
 
 if __name__ == "__main__":
     rospy.init_node("disbale_mouse")
-    joy_mouse.xinput.disableDeviceByRegexp(rospy.myargv()[1])
+    argv = rospy.myargv()
+    if "--name" == argv[1]:      #usage is disable_mouse.py --name foo
+        dev = joy_mouse.joy.lookupDeviceFileByNameAttribute(argv[2])
+    else:
+        dev = argv[1]
+    joy_mouse.xinput.disableDeviceByRegexp(dev)
--- joy_mouse/scripts/enable_mouse.py
+++ joy_mouse/scripts/enable_mouse.py
@@ -7,7 +7,14 @@ try:
 except:
     import roslib; roslib.load_manifest("joy_mouse")
     import joy_mouse.xinput
+    
+import joy_mouse.joy
 
 if __name__ == "__main__":
     rospy.init_node("enabale_mouse")
-    joy_mouse.xinput.enableDeviceByRegexp(rospy.myargv()[1])
+    argv = rospy.myargv()
+    if "--name" == argv[1]:      #usage is disable_mouse.py --name foo
+        dev = joy_mouse.joy.lookupDeviceFileByNameAttribute(argv[2])
+    else:
+        dev = argv[1]
+    joy_mouse.xinput.enableDeviceByRegexp(dev)
