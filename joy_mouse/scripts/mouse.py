078b56a2ac327e7f2b16382eb8be2afdc38e6a73
Ryohei Ueda <garaemon@gmail.com>
2015-01-07 02:37:12 +0900

    [joy_mouse] Add name API to lookup device by name like "TPPS/2 IBM TrackPoint"

--- joy_mouse/scripts/mouse.py
+++ joy_mouse/scripts/mouse.py
@@ -10,7 +10,12 @@ except:
     
 if __name__ == "__main__":
     rospy.init_node("joy_mouse")
-    joy_mouse.joy.main(rospy.get_param("~dev", "/dev/input/mouse0"),
+    device_name = rospy.get_param("~dev_name", False)
+    if device_name:
+        dev = lookupDeviceFileByNameAttribute(device_name)
+    else:
+        dev = rospy.get_param("~dev", "/dev/input/mouse0")
+    joy_mouse.joy.main(dev,
                        rospy.get_param("~autorepeat_rate", 0),
                        rospy.get_param("~frame_id", "mouse"))
     
