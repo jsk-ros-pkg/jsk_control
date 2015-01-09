527e285d7595e9ef986d3f5a8183d10057358699
YoheiKakiuchi <youhei@jsk.imi.i.u-tokyo.ac.jp>
2015-01-09 23:50:27 +0900

    add estimate params

---
+++ jsk_calibration/staro_calibration/estimate_params/calibrate_staro.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+if [ -f robot_calibrated.xml ]; then
+  echo "./robot_calibrated.xml already exists. Either back up this file or remove it before continuing"
+  exit 1
+fi
+
+echo "Checking if we can write to ./robot_calibrated.xml..."
+touch robot_calibrated.xml
+if [ "$?" -ne "0" ]; then
+  echo "Not able to write to ./robot_calibrated.xml"
+  echo "Make sure you run this script from a directory that for which you have write permissions."
+  exit 1
+fi
+rm robot_calibrated.xml
+echo "Success"
+
+# staro_calibration
+roslaunch jsk_calibration estimation_config.launch
+rosrun calibration_estimation multi_step_cov_estimator.py /tmp/staro_calibration/cal_measurements.bag /tmp/staro_calibration __name:=cal_cov_estimator
+
+est_return_val=$?
+
+if [ "$est_return_val" -ne "0" ]; then
+  echo "Estimator exited prematurely with error code [$est_return_val]"
+  exit 1
+fi
+
+# Make all the temporary files writable
+chmod ag+w /tmp/staro_calibration/*
+
