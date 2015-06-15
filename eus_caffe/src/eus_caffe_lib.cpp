e97ab0cc01d924c0fb5dda4e5a8b06d370270fc8
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2015-06-15 16:39:15 +0900

    def deep_learning_test functions

--- eus_caffe/src/eus_caffe_lib.cpp
+++ eus_caffe/src/eus_caffe_lib.cpp
@@ -1,5 +1,27 @@
-#include <stdio.h>
+#include <iostream>
+#include <memory>
+#include <random>
+#include <array>
+#include <caffe/caffe.hpp>
 
 extern "C" {
-  int hoge(int a) { return 0; }
+  int deep_learning_test (const int isize, const int dsize, double* idata, double* ddata, double* idummy, double* ddummy) {
+    //
+    caffe::SolverParameter solver_param;
+    caffe::ReadProtoFromTextFileOrDie("solver.prototxt", &solver_param);
+    std::shared_ptr<caffe::Solver<double>> solver =
+      std::shared_ptr<caffe::Solver<double>>(caffe::GetSolver<double>(solver_param));
+    boost::shared_ptr<caffe::Net<double>> net = solver->net();
+    boost::shared_ptr<caffe::MemoryDataLayer<double>> input_layer =
+      boost::dynamic_pointer_cast<caffe::MemoryDataLayer<double>>(net->layer_by_name("input"));
+    assert(input_layer);
+    input_layer->Reset(idata, idummy, isize);
+    boost::shared_ptr<caffe::MemoryDataLayer<double>> target_layer =
+      boost::dynamic_pointer_cast<caffe::MemoryDataLayer<double>>(net->layer_by_name("target"));
+    assert(target_layer);
+    target_layer->Reset(ddata, ddummy, dsize);
+    solver->Solve();
+    //
+    return 0;
+  }
 }
