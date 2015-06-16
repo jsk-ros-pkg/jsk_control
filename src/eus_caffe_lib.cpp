d60989ef9ab767bbd17750962fcc0468255ccb5a
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2015-06-16 22:02:16 +0900

    def get_ip_blob functions

--- src/eus_caffe_lib.cpp
+++ src/eus_caffe_lib.cpp
@@ -10,10 +10,9 @@ std::shared_ptr<caffe::Solver<double>> solver;
 extern "C" {
   int deep_learning_test (int isize, int dsize, double* idata, double* ddata, double* idummy, double* ddummy) {
     //
-    // caffe::SolverParameter solver_param;
     caffe::ReadProtoFromTextFileOrDie("solver.prototxt", &solver_param);
-    // std::shared_ptr<caffe::Solver<double>> solver =
     solver = std::shared_ptr<caffe::Solver<double>>(caffe::GetSolver<double>(solver_param));
+    //
     boost::shared_ptr<caffe::Net<double>> net = solver->net();
     boost::shared_ptr<caffe::MemoryDataLayer<double>> input_layer =
       boost::dynamic_pointer_cast<caffe::MemoryDataLayer<double>>(net->layer_by_name("input"));
@@ -25,6 +24,20 @@ extern "C" {
     target_layer->Reset(ddata, ddummy, dsize);
     solver->Solve();
     //
+    return 0;
+  }
+}
+
+extern "C" {
+  int get_ip_blobs (int blob_id, double* ret) {
+    boost::shared_ptr<caffe::Net<double>> net = solver->net();
+    std::vector<boost::shared_ptr<caffe::Blob<double> > > ip_blobs = net->layer_by_name("ip")->blobs();
+    std::cout << "ip_blob[" << blob_id << "]" << " size:" << ip_blobs[blob_id]->count() << " ";
+    for ( int i=0; i<ip_blobs[blob_id]->count(); i++ ){
+      ret[i] = ip_blobs[blob_id]->cpu_data()[i];
+      std::cout << ret[i] << " ";
+    }
+    std::cout << std::endl;
     return 0;
   }
 }
