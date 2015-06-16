37c68d528b379d46c6ebff69160b59f06b3ea4b7
Furushchev <furushchev@jsk.imi.i.u-tokyo.ac.jp>
2015-06-15 22:09:33 -0700

    [eus_qpoases] add dependencies to linqpOASES

--- eus_qpoases/catkin.cmake
+++ eus_qpoases/catkin.cmake
@@ -11,6 +11,8 @@ execute_process(
   RESULT_VARIABLE _make_failed)
 if (_make_failed)
   message(FATAL_ERROR "Build of qpOASES failed")
+else (_make_failed)
+  add_custom_target(qpOASES_built)
 endif(_make_failed)
 
 catkin_package()
@@ -20,9 +22,11 @@ include_directories(${CMAKE_CURRENT_BINARY_DIR}/build/qpOASES-source/include)
 
 add_executable(example1 ${PROJECT_SOURCE_DIR}/examples/example1.cpp)
 set_target_properties(example1 PROPERTIES RUNTIME_OUTPUT_DIRECTORY ${PROJECT_SOURCE_DIR}/bin)
+add_dependencies(example1 qpOASES_built)
 target_link_libraries(example1 qpOASES)
 add_library(eus_qpoases SHARED ${PROJECT_SOURCE_DIR}/src/eus_qpoases.cpp)
 set_target_properties(eus_qpoases PROPERTIES LIBRARY_OUTPUT_DIRECTORY ${PROJECT_SOURCE_DIR}/lib)
+add_dependencies(eus_qpoases qpOASES_built)
 target_link_libraries(eus_qpoases qpOASES)
 
 
