2f3519500a6c1a66a9e3ffe9ab277821f4b1bb3d
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2015-06-18 13:58:01 +0900

    comment outed pycaffe install process

--- make.sh
+++ make.sh
@@ -65,5 +65,11 @@ then
 	cp Makefile.config.example Makefile.config;
     fi
     CPU_ONLY=1 make;
+    # ## python
+    # wget -O- http://neuro.debian.net/lists/precise.jp.libre | sudo tee /etc/apt/sources.list.d/neurodebian.sources.list;
+    # sudo apt-key adv --recv-keys --keyserver hkp://pgp.mit.edu:80 0xA5D32F012649A5A9;
+    # sudo aptitude install python-skimage;
+    # CPU_ONLY=1 make pycaffe;
+    ##
     echo ">>>>> install caffe in `pwd`";
 fi
