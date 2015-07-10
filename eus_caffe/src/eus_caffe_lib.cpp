e337d3b0e810fd62f9fffa9be4ffc2c77cb03718
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2015-07-10 11:01:25 +0900

    refactor functoins names

--- eus_caffe/src/eus_caffe_lib.cpp
+++ eus_caffe/src/eus_caffe_lib.cpp
@@ -12,13 +12,13 @@ private:
   std::shared_ptr<caffe::Solver<double>> solver;
 
 public:
-  int get_blob_data (boost::shared_ptr<caffe::Blob<double> > blob, double* ret, int osize) {
+  int _get_blob_data (boost::shared_ptr<caffe::Blob<double> > blob, double* ret, int osize) {
     if ( blob == NULL ){
       std::cout << std::endl;
       std::cout << "[get_blob_data] null blob" << std::endl;
       return -1;
     }
-    std::cout << "(" << blob->count() << ") "<< " (";
+    std::cout << "[i E " << blob->count() << "] = "<< " (";
     if ( osize < 0 || osize > blob->count() ) osize = blob->count();
     for ( int i=0; i<osize; i++ ){
       ret[i] = blob->cpu_data()[i];
@@ -28,20 +28,22 @@ public:
     return 0;
   }
 
-  int get_blob_by_id_and_layer_name (std::string name, int blob_id, double* ret, int osize) {
+  int get_blob_data (char* name, double* ret, int osize) {
     boost::shared_ptr<caffe::Net<double>> net = this->solver->net();
-    std::vector<boost::shared_ptr<caffe::Blob<double> > > ip_blobs = net->layer_by_name(name)->blobs();
-    std::cout << name << "[" << blob_id << "<" << ip_blobs.size() << "]" ;
+    return _get_blob_data( net->blob_by_name(name), ret, osize);
+  }
+
+  int get_layer_blob_data (char* name, int blob_id, double* ret, int osize) {
+    boost::shared_ptr<caffe::Net<double>> net = this->solver->net();
+    const boost::shared_ptr<caffe::Layer<double>> layer = net->layer_by_name(name);
+    std::vector<boost::shared_ptr<caffe::Blob<double>>> ip_blobs = layer->blobs();
+    std::cout << "<" << layer->type() << ">" << name << "[" << blob_id << "<" << ip_blobs.size() << "]" ;
     if ( ip_blobs.size() <= blob_id ){
       std::cout << std::endl;
       std::cout << " --- too large blob_id" << std::endl;
       return -1;
     }
-    return get_blob_data(ip_blobs[blob_id], ret, osize);
-  }
-
-  int get_ip_layer_blob (int blob_id, double* ret, int osize) {
-    return get_blob_by_id_and_layer_name("ip",blob_id,ret, osize);
+    return _get_blob_data(ip_blobs[blob_id], ret, osize);
   }
 
   int create_solver (char* solver_path, char* solverstate){
@@ -82,12 +84,11 @@ public:
     //
     double buf[1];
     boost::shared_ptr<caffe::Net<double>> net = this->solver->net();
-    get_blob_data( net->blob_by_name("loss"), buf, 1);
+    get_blob_data("loss", buf, 1);
     return buf[0];
   }
 
-  int calc_forward (int inputsize, int outputsize,
-			  double* input, double* output, double* idummy){
+  int calc_forward (int inputsize, int outputsize, double* input, double* output, double* idummy){
     boost::shared_ptr<caffe::Net<double>> net = this->solver->net();
     boost::shared_ptr<caffe::MemoryDataLayer<double>> input_layer =
       boost::dynamic_pointer_cast<caffe::MemoryDataLayer<double>>(net->layer_by_name("input"));
@@ -96,7 +97,7 @@ public:
     input_layer->Reset(input, idummy, inputsize);
     net->ForwardPrefilled(nullptr);
     //
-    get_blob_data(net->blob_by_name("output"), output, outputsize);
+    get_blob_data("output", output, outputsize);
     return 0;
   }
 };
@@ -104,12 +105,11 @@ public:
 std::shared_ptr<eus_caffe> ec(new eus_caffe);
 
 extern "C" {
-  int get_blob_data (boost::shared_ptr<caffe::Blob<double> > blob, double* ret, int osize){ return ec->get_blob_data(blob,ret,osize); }
-  int get_blob_by_id_and_layer_name (std::string name, int blob_id, double* ret, int osize) { return ec->get_blob_by_id_and_layer_name(name,blob_id,ret,osize); }
-  int get_ip_layer_blob (int blob_id, double* ret, int osize) { return ec->get_ip_layer_blob(blob_id,ret,osize); }
+  int eus_caffe_get_blob_data (char* name, double* ret, int osize){ return ec->get_blob_data(name,ret,osize); }
+  int eus_caffe_get_layer_blob_data (char* name, int blob_id, double* ret, int osize) { return ec->get_layer_blob_data(name,blob_id,ret,osize); }
   //
-  int create_solver (char* solver_path, char* solverstate){ return ec->create_solver(solver_path,solverstate); }
-  int initialize_solver (int isize, int dsize, double* idata, double* ddata, double* idummy, double* ddummy){ return ec->initialize_solver(isize,dsize,idata,ddata,idummy,ddummy); }
-  double caffe_learn () { return ec->caffe_learn(); }
-  int calc_forward (int inputsize, int outputsize, double* input, double* output, double* idummy){ return ec->calc_forward(inputsize,outputsize,input,output,idummy);}
+  int eus_caffe_create_solver (char* solver_path, char* solverstate){ return ec->create_solver(solver_path,solverstate); }
+  int eus_caffe_initialize_solver (int isize, int dsize, double* idata, double* ddata, double* idummy, double* ddummy){ return ec->initialize_solver(isize,dsize,idata,ddata,idummy,ddummy); }
+  double eus_caffe_learn () { return ec->caffe_learn(); }
+  int eus_caffe_calc_forward (int inputsize, int outputsize, double* input, double* output, double* idummy){ return ec->calc_forward(inputsize,outputsize,input,output,idummy);}
 }
