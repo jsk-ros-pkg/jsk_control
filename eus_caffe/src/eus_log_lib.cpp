9e042ad008ca396824893cd0471dfb571d8f20bb
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2015-08-02 21:36:30 +0900

    remove eus-log.l from eus-caffe.l

--- eus_caffe/src/eus_log_lib.cpp
+++ eus_caffe/src/eus_log_lib.cpp
@@ -3,6 +3,7 @@
 #include <memory>
 
 std::shared_ptr<std::streambuf> eus_log_stdcout(std::cout.rdbuf());
+std::shared_ptr<std::streambuf> eus_log_stdcerr(std::cerr.rdbuf());
 std::shared_ptr<std::ofstream> eus_log_ofs;
 
 extern "C" {
@@ -15,17 +16,21 @@ extern "C" {
 	eus_log_ofs = std::shared_ptr<std::ofstream>(new std::ofstream(path));
       }
       std::cout.rdbuf(eus_log_ofs->rdbuf());
+      std::cerr.rdbuf(eus_log_ofs->rdbuf());
     } else {
       if ( eus_log_ofs ) {
 	eus_log_ofs->close();
 	eus_log_ofs = NULL;
       }
       std::cout.rdbuf(eus_log_stdcout.get());
+      std::cerr.rdbuf(eus_log_stdcerr.get());
     }
     return 0;
   }
 
   int log_test_echo(char* str){
     std::cout << str << std::endl;
+    std::cerr << str << std::endl;
+    return 0;
   }
 }
