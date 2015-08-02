d87ff8448f06cc0ca1dba95b81d4fe8fae41e9c2
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2015-08-02 20:49:01 +0900

    intiial commit of eus_log_lib.cpp

---
+++ src/eus_log_lib.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+#include <memory>
+
+std::shared_ptr<std::streambuf> eus_log_stdcout(std::cout.rdbuf());
+std::shared_ptr<std::ofstream> eus_log_ofs;
+
+extern "C" {
+  int log_change_output_stream(char* path, int size){
+    if ( path && size > 0 ){
+      if ( eus_log_ofs ) {
+	eus_log_ofs->close();
+	eus_log_ofs.reset(new std::ofstream(path));
+      } else {
+	eus_log_ofs = std::shared_ptr<std::ofstream>(new std::ofstream(path));
+      }
+      std::cout.rdbuf(eus_log_ofs->rdbuf());
+    } else {
+      if ( eus_log_ofs ) {
+	eus_log_ofs->close();
+	eus_log_ofs = NULL;
+      }
+      std::cout.rdbuf(eus_log_stdcout.get());
+    }
+    return 0;
+  }
+
+  int log_test_echo(char* str){
+    std::cout << str << std::endl;
+  }
+}
