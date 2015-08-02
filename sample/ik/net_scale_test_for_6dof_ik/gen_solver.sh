4b17e2409296fd3e43d278a694fc85fc5c8401c0
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2015-08-02 22:14:20 +0900

    initial commit of net scale dir

---
+++ sample/ik/net_scale_test_for_6dof_ik/gen_solver.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+LAYER="layer {
+  name: \"ip@depth@\"
+  type: \"InnerProduct\"
+  top: \"ip@depth@\"
+  bottom: \"sigmoid@depth_1@\"
+  param {
+    lr_mult: 1
+    decay_mult: 1
+  }
+  param {
+    lr_mult: 1
+    decay_mult: 0
+  }
+  inner_product_param {
+    num_output: 100
+    weight_filler {
+      type: \"gaussian\"
+      std: 1
+      sparse: 1
+    }
+    bias_filler {
+      type: \"constant\"
+      value: 0
+    }
+  }
+}
+layer {
+  name: \"sigmoid@depth@\"
+  type: \"Sigmoid\"
+  top: \"sigmoid@depth@\"
+  bottom: \"ip@depth@\"
+}" ;
+
+for scale in 4x50 4x100 4x200 6x50 6x100 6x200;
+do
+    DEPTH=`echo $scale | sed "s/^\(.\+\)x.\+$/\\1/g"`;
+    WIDTH=`echo $scale | sed "s/^.\+x\(.\+\)$/\\1/g"`;
+    depth=1;
+    ##
+    sed "s/@SCALE@/${scale}/g" ik_solver.prototxt > ik_solver_${scale}.prototxt;
+    sed "s/@DEPTH@/${DEPTH}/g" ik_net.prototxt > ik_net_${scale}.prototxt;
+    ##
+    while [ "$depth" -le "$DEPTH" ];
+    do
+	echo "$LAYER" | sed "s/@DEPTH@/${DEPTH}/g" | sed "s/@depth@/${depth}/g" | sed "s/@depth_1@/`expr $depth - 1`/g" >> ik_net_${scale}.prototxt;
+	depth=`expr $depth + 1`;
+    done
+done
