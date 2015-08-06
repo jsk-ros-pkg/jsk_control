1ceb3ed3d06af2c5c8517b6b8be3b68cb00c997b
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2015-08-07 08:33:27 +0900

    fix typo, solver -> net_size

--- sample/ik/net_scale_test_for_6dof_ik/analysis.sh
+++ sample/ik/net_scale_test_for_6dof_ik/analysis.sh
@@ -5,6 +5,7 @@ cd sample/ik/net_scale_test_for_6dof_ik;
 
 for net_size in `ls | grep caffemodel | sed -e "s/^ik_net_\(.\+\)_iter_3000000\.caffemodel$/\\1/g"`
 do
-    echo $solver;
-    DISPLAY="" NET_SIZE=${net_size} roseus analysis.l 1>analysis_${net_size}.log 2>&1  ;
+    echo $net_size;
+    echo "" > analysis_${net_size}.log.raw;
+    DISPLAY="" NET_SIZE=${net_size} roseus analysis.l 1>analysis_${net_size}.log.raw 2>&1  ;
 done
