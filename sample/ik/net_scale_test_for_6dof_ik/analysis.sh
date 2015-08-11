d4291c8ba6dab21f241314c1ef4685d016d97730
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2015-08-12 02:07:50 +0900

    comment in analysis

--- sample/ik/net_scale_test_for_6dof_ik/analysis.sh
+++ sample/ik/net_scale_test_for_6dof_ik/analysis.sh
@@ -12,5 +12,5 @@ do
     iteration=`echo $solver | sed -e "s/^ik_net_.\+_iter_\([0-9]\+\)\.caffemodel$/\\1/g"`;
     echo $net_size $iteration;
     echo "" > analysis_${net_size}.log.raw;
-    ## DISPLAY="" NET_SIZE=${net_size} SOLVER_ITERATION=${iteration} roseus analysis.l 1>analysis_${net_size}.log.raw 2>&1  ;
+    DISPLAY="" NET_SIZE=${net_size} SOLVER_ITERATION=${iteration} roseus analysis.l 1>analysis_${net_size}.log.raw 2>&1  ;
 done
