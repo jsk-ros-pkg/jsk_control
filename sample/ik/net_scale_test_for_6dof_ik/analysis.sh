94a9782497e9d9fa9b0fedcffc9f0fcc53e964e8
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2015-08-12 01:53:07 +0900

    add iteration arg for analysys

--- sample/ik/net_scale_test_for_6dof_ik/analysis.sh
+++ sample/ik/net_scale_test_for_6dof_ik/analysis.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
-cd `rospack find eus_caffe`;
-cd sample/ik/net_scale_test_for_6dof_ik;
+if [ ! "`ls | grep caffemodel`" ];
+then
+    cd `rospack find eus_caffe`;
+    cd sample/ik/net_scale_test_for_6dof_ik;
+fi
 
-for net_size in `ls | grep caffemodel | sed -e "s/^ik_net_\(.\+\)_iter_3000000\.caffemodel$/\\1/g"`
+for solver in `ls | grep caffemodel`
 do
-    echo $net_size;
+    net_size=`echo $solver | sed -e "s/^ik_net_\(.\+\)_iter_[0-9]\+\.caffemodel$/\\1/g"`;
+    iteration=`echo $solver | sed -e "s/^ik_net_.\+_iter_\([0-9]\+\)\.caffemodel$/\\1/g"`;
+    echo $net_size $iteration;
     echo "" > analysis_${net_size}.log.raw;
-    DISPLAY="" NET_SIZE=${net_size} roseus analysis.l 1>analysis_${net_size}.log.raw 2>&1  ;
+    ## DISPLAY="" NET_SIZE=${net_size} SOLVER_ITERATION=${iteration} roseus analysis.l 1>analysis_${net_size}.log.raw 2>&1  ;
 done
