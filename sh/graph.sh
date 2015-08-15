45d54a6d1e5bfaa5ea3ec65c8ec9ea0fc1506e62
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2015-08-15 16:27:26 +0900

    xtics scaling

--- sh/graph.sh
+++ sh/graph.sh
@@ -2,23 +2,32 @@
 
 function gen_loss_graph(){
     log=$1;
+    xtics_size=1;
+    last_x=`tail ${log} --lines=1000 | grep -e "^.\+Iteration \([0-9]\+\), loss = \([0-9\.]\+\)$" | tail -1 | sed -e "s/^.\+Iteration \([0-9]\+\), loss = \([0-9\.]\+\)$/\\1/g"`;
+    xtics_scale=`echo "${last_x} / ${xtics_size}" | bc`;
     ## gen log data for graph
+    echo "output data -> /tmp/${log}.dat";
     echo -n "" > /tmp/${log}.dat ; ID=0;
-    for d in `grep ": loss =" ${log}  | sed -e "s/^.\+: loss = \([^ ]\+\) .\+$/\\1/g"`; do
-        echo $ID $d >> /tmp/${log}.dat ; ID=`expr $ID + 1`;
+    cat ${log}  | grep -e "^.\+Iteration \([0-9]\+\), loss = \([0-9\.]\+\)$" | sed -e "s/^.\+Iteration \([0-9]\+\), loss = \([0-9\.]\+\)$/\\1 \\2/g" | while read d; do
+	id=`echo ${d} | sed -e "s/^\(.\+\) .\+$/\\1/g"`;
+	val=`echo ${d} | sed -e "s/^.\+ \(.\+\)$/\\1/g"`;
+	id2=`echo "$id / ${xtics_scale}" | bc -l`;
+        echo $id2 $val >> /tmp/${log}.dat ; ID=`expr $ID + 1`;
     done
+    echo " --- output data done, start plotting";
     ##
     gnuplot <<EOF
 set terminal postscript eps color enhanced
 set output "${log}.eps"
-set xlabel "STEP"
+set xlabel "STEP/${xtics_scale}"
 set ylabel "LOSS"
 set title "loss/step for ${log}"
 set yrange [ 0 : 1 ]
 set mxtics 5
 set mytics 5
-set xtics 100
+set xtics 0.5
 set ytics 0.5
 plot "/tmp/${log}.dat" using 1:2 notitle w l
 EOF
+    echo " --- plotting done";
 }
