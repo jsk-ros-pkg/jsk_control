0a3f01ec494b364fd64244e19d03ebd18e9396f2
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2015-08-16 13:37:41 +0900

    font type3 forbitted

--- sample/ik/net_scale_test_for_redundancy_ik/hist.py
+++ sample/ik/net_scale_test_for_redundancy_ik/hist.py
@@ -117,7 +117,7 @@ def calc_cosh(xl, loc=0, scale=1):
 def gen_graph(ls, rng, gen_data, title="histgram", xlabel="dif [m]", labels=None, logpath="log.eps", draw_np=True):
     plt.title(title)
     plt.xlabel(xlabel)
-    plt.ylabel("histgram")
+    plt.ylabel(".")
     plt.grid(True)
     if not labels:
         labels = ls
@@ -143,9 +143,10 @@ def gen_graph(ls, rng, gen_data, title="histgram", xlabel="dif [m]", labels=None
             ##
             loc=float(np.mean(time_buf))
             scale=float(np.std(time_buf))
+            phi = scale/2
             x = np.arange(rng[0], rng[1], (rng[1] - rng[0])/1000.0)
             if draw_np:
-                plt.plot(x, calc_cosh(x, loc=loc, scale=scale), label=(labels[i]+".nd (" + (r'''$\mu=%.1f$, $\sigma=%.2f$''' % (loc, scale)) + ")"), marker=markers[(i % len(markers))])
+                plt.plot(x, calc_lap(x, loc=loc, scale=scale), label=("Laplace" + "\n" + ('     $\mu=%.1f$\n     $\phi=%.2f$' % (loc, phi))), marker=markers[(i % len(markers))])
             i = i+1
             ##plt.hist(time_buf_buf, bins=100, label=labels, range=rng, normed=True)
     plt.legend()
@@ -154,29 +155,38 @@ def gen_graph(ls, rng, gen_data, title="histgram", xlabel="dif [m]", labels=None
 
 params = {'backend': 'ps',
           'axes.labelsize': 20,
+          'axes.unicode_minus': False,
           'text.fontsize': 20,
           'legend.fontsize': 20,
           'xtick.labelsize': 15,
           'ytick.labelsize': 15,
           "figure.subplot.right": 0.96,
           "figure.subplot.top": 0.96,
-          "figure.subplot.left": 0.08,
-          "figure.subplot.bottom": 0.1,
-          'figure.figsize': [15, 8],
+          "figure.subplot.left": 0.12,
+          "figure.subplot.bottom": 0.15,
+          'figure.figsize': [7, 4.5],
           'ps.useafm': True,
-          'pdf.use14corefonts': True
+          'pdf.use14corefonts': True,
+          'text.usetex': True
           }
 plt.rcParams.update(params)
 
-gen_graph(["torque_analysis.objective.dat"], [50, 150], [functools.partial(gen_torque_buf, 2)], xlabel='Torque/MAX', title="_", labels=["RMAP/IK"], logpath="torque_rate.pdf")
-## gen_graph(["torque_analysis.objective.dat", "torque_analysis.objective.dat", "torque_analysis.objective.dat"], [50, 150], [functools.partial(gen_torque_buf, 0), functools.partial(gen_torque_buf, 1), functools.partial(gen_torque_buf, 2)], xlabel='Torque/MAX', title="_", labels=["IK", "RMAP", "RMAP/IK"], logpath="torque_rate.pdf")
+# dat="torque_analysis.6x200.NESTEROV.log.test"
+# gen_graph([dat], [50, 150], [functools.partial(gen_torque_buf, 2)], xlabel='Torque/MAX', title="_", labels=["RMAP/IK"], logpath="torque_rate.pdf")
+# gen_graph([dat, dat], [50, 150], [functools.partial(gen_torque_buf, 0), functools.partial(gen_torque_buf, 1)], xlabel='Torque/MAX', title="_", labels=["IK", "RMAP", "RMAP/IK"], logpath="torque_rate2.pdf")
+
+ls=["analysis.6x200.NESTEROV.log.test"]
+lb=["5x200"]
+gen_graph(ls, [50, 150], gen_time_buf, xlabel='time [micro sec]', title=".", labels=lb, logpath="hist_time_wide.pdf", draw_np=False)
+for i in [0, 1, 2]:
+    print "diff pos["+str(i)+"] histgram"
+    gen_graph(ls, [-40, 40], functools.partial(gen_pos_buf, i), xlabel='dif [mm]', title="_", labels=[("diff pos["+str(i)+"]")], logpath=("hist_dpos" + str(i) + "_wide.pdf"))
+for i in [0, 1, 2]:
+    print "diff rpy[" + str(i) + "] histgram"
+    gen_graph(ls, [-40, 40], functools.partial(gen_rot_buf, i), xlabel='dif [deg]', title="_", labels=[("diff rpy[" + str(i) + "]")], logpath=("hist_drpy" + str(i) + "_wide.pdf"))
 
 # ls=["analysis.6x200.NESTEROV.log.test"]
-# lb=["6x200"]
-# gen_graph(ls, [50, 150], gen_time_buf, xlabel='time [micro sec]', title="_", labels=lb, logpath="hist_time_wide.pdf", draw_np=False)
-# for i in [0, 1, 2]:
-#     print "diff pos["+str(i)+"] histgram"
-#     gen_graph(ls, [-50, 50], functools.partial(gen_pos_buf, i), xlabel='dif [mm]', title="_", labels=lb, logpath=("hist_dpos" + str(i) + "_wide.pdf"))
-# for i in [0, 1, 2]:
-#     print "diff rpy[" + str(i) + "] histgram"
-#     gen_graph(ls, [-50, 50], functools.partial(gen_rot_buf, i), xlabel='dif [rad]', title="_", labels=lb, logpath=("hist_drpy" + str(i) + "_wide.pdf"))
+# lb=["5x200"]
+# gen_graph(ls, [50, 150], gen_time_buf, xlabel='time [micro sec]', title="_", labels=lb, logpath="hist_time.pdf", draw_np=True)
+# gen_graph(ls, [-50, 50], gen_pos_buf2, xlabel='dif [mm]', title="_", labels=lb, logpath=("hist_dpos.pdf"))
+# gen_graph(ls, [-50, 50], gen_rot_buf2, xlabel='dif [deg]', title="_", labels=lb, logpath=("hist_drot.pdf"))
