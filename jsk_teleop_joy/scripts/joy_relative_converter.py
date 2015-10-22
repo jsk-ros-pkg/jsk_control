1704b24d2b96aae962e4c87968f68078442417a2
Satoshi Iwaishi <iwaishi@jsk.imi.i.u-tokyo.ac.jp>
2015-10-22 11:44:02 +0900

    [jsk_teleop_joy/scripts/joy_relative_converter.py] fix bug to support buttons

--- jsk_teleop_joy/scripts/joy_relative_converter.py
+++ jsk_teleop_joy/scripts/joy_relative_converter.py
@@ -52,7 +52,9 @@ class AxesConverter:
     return rel_axes
 
   def __convertAxis(self, abs_axis, in_origin, out_origin):
-    if in_origin is None:
+    if abs_axis == 0.0 or abs_axis == 1.0:
+      rel_axis = abs_axis
+    elif in_origin is None:
       rel_axis = out_origin
     elif abs_axis < in_origin:
       rel_axis = (((abs_axis - 0.0) /  (in_origin - 0.0)) * (out_origin - 0.0)) + 0.0
