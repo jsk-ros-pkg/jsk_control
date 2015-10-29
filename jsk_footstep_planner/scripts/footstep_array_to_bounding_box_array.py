e6750adf74bbf259c2dd53239c0c1497fc32d386
Yusuke Oshiro <oshiro@jsk.imi.i.u-tokyo.ac.jp>
2015-10-29 16:00:05 +0900

    add variables to modify bounding box height

--- jsk_footstep_planner/scripts/footstep_array_to_bounding_box_array.py
+++ jsk_footstep_planner/scripts/footstep_array_to_bounding_box_array.py
@@ -12,11 +12,15 @@ def callback(msg):
         box.header = msg.header
         box.pose = footstep.pose
         box.dimensions = footstep.dimensions
+        box.pose.position.z += (z_max + z_min) / 2.0
+        box.dimensions.z = z_max - z_min
         box_array.boxes.append(box)
     pub.publish(box_array)
 
 if __name__ == "__main__":
     rospy.init_node("footstep_array_to_bounding_box")
+    z_max = rospy.get_param('~z_max',0.0005)
+    z_min = rospy.get_param('~z_min',-0.0005)
     pub = rospy.Publisher("~output", BoundingBoxArray)
     sub = rospy.Subscriber("~input", FootstepArray, callback)
     rospy.spin()
