b87a49fc6ded83b11a4110d4322d29c54d2354aa
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2015-12-04 22:43:21 +0900

    update dir config

--- sample/scalable_test/gen_test_dir.sh
+++ sample/scalable_test/gen_test_dir.sh
@@ -2,17 +2,36 @@
 
 DATE=`date +"%M%I%d%m%Y"`
 
-mkdir -p "tmp.${DATE}";
-rm -rf latest.tmp;
-ln -s "tmp.${DATE}" latest.tmp;
-cd "tmp.${DATE}";
+if [ ! -e "gen_test_dir.sh" ];
+then
+    if [ "`rospack find eus_caffe`" ];
+    then
+	roscd eus_caffe;
+    elif [ "`locate eus_caffe | grep "src" | grep -e "eus_caffe$" | tail -1`" ];
+    then
+	cd `locate eus_caffe | grep "src" | grep -e "eus_caffe$" | tail -1`
+    fi
+    if [ ! -e "gen_test_dir.sh" ];
+    then
+	echo -e "\e[31meus_caffe not found\e[m"
+	exit -1;
+    fi
+    cd sample/scalable_test;
+fi
 
-ln -s ../learn.l;
-ln -s ../teacher;
-cp ../ik_net.prototxt .;
-cp ../ik_solver.prototxt .;
-cp ../predict_ik_net.prototxt .;
+ORG_DIR=`pwd`;
+TMP_DIR="tmp/${DATE}";
+mkdir -p $TMP_DIR;
+rm -rf latest.tmp;
+ln -s $TMP_DIR latest.tmp;
+cd $TMP_DIR;
+
+ln -s $ORG_DIR/learn.l;
+ln -s $ORG_DIR/teacher;
+cp $ORG_DIR/ik_net.prototxt .;
+cp $ORG_DIR/ik_solver.prototxt .;
+cp $ORG_DIR/predict_ik_net.prototxt .;
 
 roseus learn.l "(progn (ik-learn) (exit))" > log.learn 2>&1;
 
-cd --;
+cd $ORG_DIR;
