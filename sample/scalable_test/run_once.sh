2da96d64f387f2d04beca2f934119e191ebd90dc
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2015-12-11 19:19:00 +0900

    add nan killer

--- sample/scalable_test/run_once.sh
+++ sample/scalable_test/run_once.sh
@@ -19,7 +19,7 @@ then
     cd sample/scalable_test;
 fi
 
-DATE=`date +"%S%M%I%d%m%Y"`
+DATE="`date +"%S%M%I%d%m%Y"`_$$"
 ORG_DIR=`pwd`;
 echo -e "\e[33morg_dir: ${ORG_DIR}\e[m";
 TMP_DIR="tmp/${DATE}";
@@ -43,10 +43,32 @@ cp $ORG_DIR/predict_ik_net.prototxt .;
 
 echo -e "\e[33mgen solver ... \e[m";
 ./gen_solver.sh;
+
+echo -e "\e[33mstart nan killer ...\e[m";
+# ( touch log.learn; while [ ! "`tail log.learn | grep \" = \" | grep \"nan\|inf\"`" ]; do sleep 3; done; echo -e "\e[31mnan detected\e[m"; echo -e "\e[31mkill $$\e[m"; kill $$;  ) &
+(
+    sleep 10;
+    PID=`lsof log.learn 2> /dev/null | grep irteusgl | sed -e "s/^irteusgl \([0-9]\+\) .\+$/\\1/g"`;
+    while [ "$PID" ];
+    do
+        PID=`lsof log.learn 2> /dev/null | grep irteusgl | sed -e "s/^irteusgl \([0-9]\+\) .\+$/\\1/g"`;
+        if [ "`tail log.learn | grep \" = \" | grep \"nan\|inf\"`" ];
+        then
+            echo -e "\e[31mnan detected\e[m";
+            echo -e "\e[31mkill $PID\e[m"
+            kill $PID;
+        fi
+        sleep 3;
+    done
+) &
+
 echo -e "\e[33mrun learning ... \e[m";
-roseus learn.l "(bench (progn (print (ik-learn-with-all-solver)) (print (ik-learn-best-loop :max_iter 100000 :lp 30))))" "(exit)" > log.learn 2>&1;
+roseus learn.l "(bench (progn (print (ik-learn-with-all-solver)) (print (ik-learn-best-loop :max_iter 100000 :lp 40 :vain-cnt-max 20))))" "(exit)" > log.learn 2>&1;
+
 echo -e "\e[33m${TMP_DIR} done\e[m";
 tail log.learn;
+
 head ik_net.prototxt -n 1;
 
 cd $ORG_DIR;
+
