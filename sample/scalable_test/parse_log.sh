f5f20850a1719a9f746c34f63c72c0c8bb3c9084
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2015-12-15 10:46:47 +0900

    initial commit of parse_log.sh

---
+++ sample/scalable_test/parse_log.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+for _p in `ls tmp`;
+do
+    p="tmp/${_p}";
+    if [ -e "${p}/log.learn" ];
+    then
+        echo -e "\e[36m ----------- \e[m";
+        echo -e "\e[36m`head ${p}/predict_ik_net.prototxt -n 1`\e[m";
+        echo -n -e "\e[36m${p}/log.learn:  ";
+        cat ${p}/log.learn | grep vain | grep now \
+            | sed -e "s/^.\+now: \([0-9\.+-e]\+\)$/\\1/g" \
+            | awk '{if (NR==1) min=$1} {if($1 < min) min=$1} END {print min}';
+        echo -n -e "\e[m";
+        cat ${p}/log.learn | grep vain | grep now | awk '{print "  " $0}';
+    fi
+done
+
