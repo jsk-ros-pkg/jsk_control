75516a8e673cba443528a29f3dbc10bba24a4424
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2015-12-15 10:54:19 +0900

    use cd

--- sample/scalable_test/parse_log.sh
+++ sample/scalable_test/parse_log.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-for _p in `ls tmp`;
+for p in `ls`;
 do
-    p="tmp/${_p}";
+    ## p="${_p}";
     if [ -e "${p}/log.learn" ];
     then
         echo -e "\e[36m ----------- \e[m";
         echo -e "\e[36m`head ${p}/predict_ik_net.prototxt -n 1`\e[m";
-        echo -n -e "\e[36m${p}/log.learn:  ";
+        echo -n -e "\e[36m${p}:  ";
         cat ${p}/log.learn | grep vain | grep now \
             | sed -e "s/^.\+now: \([0-9\.+-e]\+\)$/\\1/g" \
             | awk '{if (NR==1) min=$1} {if($1 < min) min=$1} END {print min}';
