94ae5289770054c594190512068621e430e4c39b
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2016-01-01 16:47:21 +0900

    able to set base_lr_list

--- sample/scalable_test/gen_solver.sh
+++ sample/scalable_test/gen_solver.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
 
-for base_lr in 0.01 0.005 0.001;
+if [ ! "$BASE_LR_LIST" ];
+then
+    if [ "$1" ];
+    then
+        BASE_LR_LIST=$1;
+    else
+        BASE_LR_LIST="0.004 0.002 0.001";
+    fi
+fi
+
+for base_lr in $BASE_LR_LIST;
 do
     for id in 0 1 2;
     do
-	echo -e "\e[33mbase_lr=${base_lr} x ${id}\e[m";
-	roseus gen-solver \
-	    "(gen-solver :path \"ik_solver_${base_lr}blr_${id}id.prototxt\" :base_lr ${base_lr} :snapshot_prefix \"\\\"ik_net_${base_lr}blr_${id}id\\\"\")" \
-	    "(exit)" > /dev/null 2>&1;
+        echo -e "\e[33mbase_lr=${base_lr} x ${id}\e[m";
+        roseus gen-solver \
+            "(gen-solver :path \"ik_solver_${base_lr}blr_${id}id.prototxt\" :base_lr ${base_lr} :snapshot_prefix \"\\\"ik_net_${base_lr}blr_${id}id\\\"\")" \
+            "(exit)" > /dev/null 2>&1;
     done
 done
