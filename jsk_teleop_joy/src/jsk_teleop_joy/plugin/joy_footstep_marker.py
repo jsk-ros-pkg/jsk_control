abae060fd786f5b617df9b74e78c1a3c786a4f73
orikuma <ik0313@gmail.com>
2016-03-24 16:21:34 +0900

    [jsk_teleop_joy] Add footstep marker synchonization to joy_footstep_marker

--- jsk_teleop_joy/src/jsk_teleop_joy/plugin/joy_footstep_marker.py
+++ jsk_teleop_joy/src/jsk_teleop_joy/plugin/joy_footstep_marker.py
@@ -8,10 +8,12 @@ except:
 
 import actionlib
 from joy_pose_6d import JoyPose6D
+from jsk_interactive_marker.srv import GetTransformableMarkerPose
 from geometry_msgs.msg import PoseStamped
 from std_srvs.srv import Empty
 import copy
 import time
+import tf
 
 def isSamePose(pose1, pose2):
   return all([getattr(pose1.position, attr) == getattr(pose2.position, attr) for attr in ["x", "y", "z"]]) and all ([getattr(pose1.orientation, attr) == getattr(pose2.orientation, attr) for attr in ["x", "y", "z", "w"]])
@@ -22,14 +24,28 @@ class JoyFootstepMarker(JoyPose6D):
     JoyPose6D.__init__(self, name, args)
     self.pose_pub = rospy.Publisher(self.getArg('pose', 'pose'), PoseStamped)
     self.current_goal_pose = None
+    self.use_tf = self.getArg('use_tf', True)
   
     # make service proxy
     rospy.wait_for_service('/footstep_marker/reset_marker')
     self.reset_marker_srv = rospy.ServiceProxy('/footstep_marker/reset_marker', Empty)
     rospy.wait_for_service('/footstep_marker/execute_footstep')
     self.execute_footstep_srv = rospy.ServiceProxy('/footstep_marker/execute_footstep', Empty)
-    
+    rospy.wait_for_service('/footstep_marker/get_footstep_marker_pose')
+    self.get_footstep_marker_pose_srv = rospy.ServiceProxy('/footstep_marker/get_footstep_marker_pose', GetTransformableMarkerPose)
+
+    # initialize maker pose
+    marker_pose = self.getCurrentMarkerPose()
+    if marker_pose != None:
+      self.pre_pose = marker_pose 
   def joyCB(self, status, history):
+    # synchronize marker_pose before publish pre_pose
+    now = rospy.Time.from_sec(time.time())    
+    if (now - self.prev_time).to_sec() > 1.0 / 10.0:
+      marker_pose = self.getCurrentMarkerPose()
+      if marker_pose != None and not isSamePose(self.pre_pose.pose, marker_pose.pose):
+        self.pre_pose = marker_pose
+    
     JoyPose6D.joyCB(self, status, history)
     latest = history.latest()
     if not latest:
@@ -41,10 +57,20 @@ class JoyFootstepMarker(JoyPose6D):
       self.pre_pose = PoseStamped()
       self.pre_pose.pose.orientation.w = 1
 
-    if self.current_goal_pose == None or not isSamePose(self.current_goal_pose.pose, self.pre_pose.pose):
-      now = rospy.Time.from_sec(time.time())
-      # placement.time_from_start = now - self.prev_time
+    if self.current_goal_pose == None or not isSamePose(self.current_goal_pose.pose, self.pre_pose.pose):      
       if (now - self.prev_time).to_sec() > 1 / 30.0:
         self.pose_pub.publish(self.pre_pose)
         self.prev_time = now
         self.current_goal_pose = copy.deepcopy(self.pre_pose)
+  def getCurrentMarkerPose(self):
+    try:      
+      marker_pose = self.get_footstep_marker_pose_srv("movable_footstep_marker").pose_stamped
+      if self.use_tf:
+        marker_pose = self.tf_listener.transformPose(self.frame_id, marker_pose)
+      return marker_pose
+    except rospy.ServiceException, e:
+      rospy.logwarn("Failed to get initial marker pose: %s", e)
+      return None
+    except tf.LookupException, e:
+      rospy.logwarn("Failed to lookup tf: %s", e)
+      return None
