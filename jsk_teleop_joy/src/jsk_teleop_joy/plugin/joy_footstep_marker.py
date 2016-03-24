f6fd4a8269f7b7902468c4baf0bc31d447ab9357
leus <leus@jzou.(none)>
2016-03-24 17:10:04 +0900

    [jsk_teleop_joy] Add marker_name arg to getCurrentMarkerPose and initialize marker by initial_footstep_marker in reset process

--- jsk_teleop_joy/src/jsk_teleop_joy/plugin/joy_footstep_marker.py
+++ jsk_teleop_joy/src/jsk_teleop_joy/plugin/joy_footstep_marker.py
@@ -25,6 +25,7 @@ class JoyFootstepMarker(JoyPose6D):
     self.pose_pub = rospy.Publisher(self.getArg('pose', 'pose'), PoseStamped)
     self.current_goal_pose = None
     self.use_tf = self.getArg('use_tf', True)
+    self.sync_rate = self.getArg('sync_rate', 10)
   
     # make service proxy
     rospy.wait_for_service('/footstep_marker/reset_marker')
@@ -35,14 +36,14 @@ class JoyFootstepMarker(JoyPose6D):
     self.get_footstep_marker_pose_srv = rospy.ServiceProxy('/footstep_marker/get_footstep_marker_pose', GetTransformableMarkerPose)
 
     # initialize maker pose
-    marker_pose = self.getCurrentMarkerPose()
+    marker_pose = self.getCurrentMarkerPose("movable_footstep_marker")
     if marker_pose != None:
       self.pre_pose = marker_pose 
   def joyCB(self, status, history):
     # synchronize marker_pose before publish pre_pose
     now = rospy.Time.from_sec(time.time())    
-    if (now - self.prev_time).to_sec() > 1.0 / 10.0:
-      marker_pose = self.getCurrentMarkerPose()
+    if (now - self.prev_time).to_sec() > 1.0 / self.sync_rate:
+      marker_pose = self.getCurrentMarkerPose("movable_footstep_marker")
       if marker_pose != None and not isSamePose(self.pre_pose.pose, marker_pose.pose):
         self.pre_pose = marker_pose
     
@@ -50,21 +51,25 @@ class JoyFootstepMarker(JoyPose6D):
     latest = history.latest()
     if not latest:
       return
-    if status.circle and not latest.circle:
+    if status.circle and not latest.circle: # execute footsteps
       self.execute_footstep_srv()
-    elif status.cross and not latest.cross:
+    elif status.cross and not latest.cross: # reset marker
       self.reset_marker_srv()
-      self.pre_pose = PoseStamped()
-      self.pre_pose.pose.orientation.w = 1
+      marker_pose = self.getCurrentMarkerPose("initial_footstep_marker")
+      if marker_pose != None:
+        self.pre_pose = marker_pose
+      else:
+        self.pre_pose = PoseStamped()
+        self.pre_pose.pose.orientation.w = 1
 
-    if self.current_goal_pose == None or not isSamePose(self.current_goal_pose.pose, self.pre_pose.pose):      
+    if self.current_goal_pose == None or not isSamePose(self.current_goal_pose.pose, self.pre_pose.pose):
       if (now - self.prev_time).to_sec() > 1 / 30.0:
         self.pose_pub.publish(self.pre_pose)
         self.prev_time = now
         self.current_goal_pose = copy.deepcopy(self.pre_pose)
-  def getCurrentMarkerPose(self):
+  def getCurrentMarkerPose(self, marker_name):
     try:      
-      marker_pose = self.get_footstep_marker_pose_srv("movable_footstep_marker").pose_stamped
+      marker_pose = self.get_footstep_marker_pose_srv(marker_name).pose_stamped
       if self.use_tf:
         marker_pose = self.tf_listener.transformPose(self.frame_id, marker_pose)
       return marker_pose
