959a4f6f2aa9edf80b2b3112ab389faf54a054be
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2016-04-30 14:49:08 +0900

    find test dir fixed

--- sample/scalable_test_trajectory/backup_tmp.sh
+++ sample/scalable_test_trajectory/backup_tmp.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ ! "`basename \`pwd\``" == "scalable_test" ]
+if [ ! "`basename \`pwd\``" == "scalable_test_trajectory" ]
 then
     if [ "`rospack find eus_caffe`" ];
     then
@@ -9,14 +9,13 @@ then
     then
 	cd `locate eus_caffe | grep "src" | grep -e "eus_caffe$" | tail -1`
     fi
+    cd sample/scalable_test_trajectory;
     ##
-    if [ !  "`basename \`pwd\``" == "scalable_test" ];
+    if [ !  "`basename \`pwd\``" == "scalable_test_trajectory" ];
     then
 	echo -e "\e[31meus_caffe not found\e[m"
 	exit -1;
     fi
-    ##
-    cd sample/scalable_test;
 fi
 
 ROOT=$1;
