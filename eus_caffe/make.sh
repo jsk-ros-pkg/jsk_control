e174d4968978a8b05d795d1b41bc255b401da5e8
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2016-10-18 00:36:51 +0900

    custom compiler specified

--- eus_caffe/make.sh
+++ eus_caffe/make.sh
@@ -81,7 +81,7 @@ then
     if [ ! -e "Makefile.config" ];
     then
 	_mlog "gen caffe build config";
-	cat Makefile.config.example | sed -e "s#/usr/local/lib#/usr/local/lib ${DIR_ROOT}/3rdparty/gflags/build/lib ${DIR_ROOT}/3rdparty/mdb/libraries/liblmdb ${DIR_ROOT}/3rdparty/glog-0\.3\.3/\.libs#g" | sed -e "s#/usr/local/include#/usr/local/include ${DIR_ROOT}/3rdparty/gflags/build/include#g" > Makefile.config;
+	cat Makefile.config.example | sed -e "s#/usr/local/lib#/usr/local/lib ${DIR_ROOT}/3rdparty/gflags/build/lib ${DIR_ROOT}/3rdparty/mdb/libraries/liblmdb ${DIR_ROOT}/3rdparty/glog-0\.3\.3/\.libs#g" | sed -e "s#/usr/local/include#/usr/local/include ${DIR_ROOT}/3rdparty/gflags/build/include#g" | sed -e "s/# CUSTOM_CXX := g++/CUSTOM_CXX := g++/g" > Makefile.config;
 	cat Makefile.config;
     fi
     _mlog "build caffe";
