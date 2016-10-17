bf77b2d8b0cf93c625b1156a00ecfe797a968eae
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2016-10-17 23:17:04 +0900

    add log message in build process

--- eus_caffe/make.sh
+++ eus_caffe/make.sh
@@ -2,9 +2,17 @@
 
 DIR_3rdparty="";
 DIR_ROOT="";
+LOG_HEAD="_/_/_/_/_/_/_/_/_/";
+
+function _mlog () {
+    echo $LOG_HEAD;
+    echo $1;
+    echo $LOG_HEAD;
+}
 
 if [ ! -e "3rdparty" ];
 then
+    _mlog "gen 3rdparty dir";
     mkdir 3rdparty;
 fi
 cd 3rdparty;
@@ -13,6 +21,7 @@ if [ ! -e "caffe" ];
 then
     ##
     ##
+    _mlog "download caffe";
     wget https://github.com/BVLC/caffe/archive/d362894887af9dca8581906b2284f5be81dbd403.zip
     unzip d362894887af9dca8581906b2284f5be81dbd403.zip;
     mv caffe-d362894887af9dca8581906b2284f5be81dbd403 caffe;
@@ -28,45 +37,54 @@ then
     DIR_3rdparty=`pwd`;
     ##
     ## glog
+    _mlog "download glog";
     ## wget https://google-glog.googlecode.com/files/glog-0.3.3.tar.gz
     wget https://github.com/google/glog/archive/0b0b022be1c9c9139955af578fe477529d4b7b3c.zip;
     mv 0b0b022be1c9c9139955af578fe477529d4b7b3c.zip glog-0.3.3.zip;
     unzip glog-0.3.3.zip;
     mv glog-0b0b022be1c9c9139955af578fe477529d4b7b3c glog-0.3.3;
     cd glog-0.3.3;
+    _mlog "build glog";
     ./configure;
     make;
     ## make && make install;
     cd $DIR_3rdparty;
-    echo ">>>>> install glog in `pwd`";
+    _mlog "done glog";
     ##
     ## gflags
+    _mlog "download gflag";
     wget https://github.com/gflags/gflags/archive/d4e971c10b1557292b5371807a23921d15e7fece.zip;
     unzip d4e971c10b1557292b5371807a23921d15e7fece.zip;
     mv gflags-d4e971c10b1557292b5371807a23921d15e7fece gflags;
     cd gflags;
+    _mlog "build gflag";
     mkdir build && cd build
     export CXXFLAGS="-fPIC" && cmake .. && make VERBOSE=1
     ## make && make install
     make;
     cd $DIR_3rdparty;
-    echo ">>>>> install gflags in `pwd`";
+    _mlog "done gflag";
     ##
     ## lmdb
+    _mlog "download lmdb";
     wget https://github.com/LMDB/lmdb/archive/7e476e4983cfba45cefe793b8bd6e13c486b3989.zip;
     unzip 7e476e4983cfba45cefe793b8bd6e13c486b3989.zip;
     mv lmdb-7e476e4983cfba45cefe793b8bd6e13c486b3989 mdb;
     cd mdb/libraries/liblmdb;
+    _mlog "build lmdb";
     make;
     ## make && make install
     cd $DIR_3rdparty;
-    echo ">>>>> install lmb in `pwd`";
+    _mlog "done lmdb";
     ##
     cd $DIR_ROOT;
     if [ ! -e "Makefile.config" ];
     then
+	_mlog "gen caffe build config";
 	cat Makefile.config.example | sed -e "s#/usr/local/lib#/usr/local/lib ${DIR_ROOT}/3rdparty/gflags/build/lib ${DIR_ROOT}/3rdparty/mdb/libraries/liblmdb ${DIR_ROOT}/3rdparty/glog-0\.3\.3/\.libs#g" | sed -e "s#/usr/local/include#/usr/local/include ${DIR_ROOT}/3rdparty/gflags/build/include#g" > Makefile.config;
+	cat Makefile.config;
     fi
+    _mlog "build caffe";
     CPU_ONLY=1 make;
     # ## python
     if [ "$PYCAFFE_INSTALL" ];
@@ -79,5 +97,5 @@ then
 	CPU_ONLY=1 make pycaffe;
     fi
     ##
-    echo ">>>>> install caffe in `pwd`";
+    _mlog "done caffe";
 fi
