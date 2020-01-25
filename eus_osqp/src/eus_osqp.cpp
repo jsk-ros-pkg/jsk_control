6c400f7ac44da5b79e3da80af72a627bb1a4cb4c
Naoki-Hiraoka <hiraoka@jsk.imi.i.u-tokyo.ac.jp>
2020-01-25 21:08:47 +0900

    reduce calculation cost

--- eus_osqp/src/eus_osqp.cpp
+++ eus_osqp/src/eus_osqp.cpp
@@ -193,58 +193,22 @@ class osqp_solver {
 public:
   osqp_solver(size_t _state_len,
               size_t _inequality_len,
-              const std::vector<double>& _Psparse,// have to be symmetric
-              const std::vector<double>& _Asparse,// have to be symmetric
+              csc* _P,// have to be symmetric
+              csc* _A,// have to be symmetric
               int verbose)
     : state_len(_state_len),
       inequality_len(_inequality_len),
-      Psparse(_Psparse),
-      Asparse(_Asparse),
-      P_nnz(Psparse.size() - std::count(Psparse.begin(), Psparse.end(), 0)),
-      P_x(new c_float[P_nnz]),
-      P_i(new c_int[P_nnz]),
-      P_p(new c_int[state_len+1]),
-      P(csc_matrix(state_len, state_len, P_nnz, P_x, P_i, P_p)),//(no MALLOC to create inner arrays x, i, p)
+      P(_P),
       q(new c_float[state_len]),
-      A_nnz(Asparse.size() - std::count(Asparse.begin(), Asparse.end(), 0)),
-      A_x(new c_float[A_nnz]),
-      A_i(new c_int[A_nnz]),
-      A_p(new c_int[state_len+1]),
-      A(csc_matrix(inequality_len, state_len, A_nnz, A_x, A_i, A_p)),
+      A(_A),
       l(new c_float[inequality_len]),
       u(new c_float[inequality_len]),
       settings((OSQPSettings *)c_malloc(sizeof(OSQPSettings))),
       data((OSQPData *)c_malloc(sizeof(OSQPData))),
       work(NULL)
   {
-    P_p[0] = 0;
-    for (size_t j = 0; j < state_len; j++) {
-      size_t num=0;
-      for(size_t i = 0; i < state_len; i++){
-        if(Psparse[i*state_len+j]!=0){
-          P_i[P_p[j]+num] = i;
-          P_x[P_p[j]+num] = 0.0;
-          num++;
-        }
-      }
-      P_p[j+1] = P_p[j] + num;
-    }
-
     for (c_int i = 0; i < state_len; i++) {
       q[i] = 0.0;
-    }
-
-    A_p[0] = 0;
-    for (size_t j = 0; j < state_len; j++) {
-      size_t num=0;
-      for(size_t i = 0; i < inequality_len; i++){
-        if(Asparse[i*state_len+j]!=0){
-          A_i[A_p[j]+num] = i;
-          A_x[A_p[j]+num] = 0.0;
-          num++;
-        }
-      }
-      A_p[j+1] = A_p[j] + num;
     }
 
     for(size_t j = 0; j < inequality_len ; j++){
@@ -274,13 +238,14 @@ public:
 
     if(osqp_setup(&work, data, settings) == 0) initialized = true;
     else initialized = false;
+
     return;
   }
 
   ~osqp_solver(){
     osqp_cleanup(work);
-    csc_spfree(P); // free P_x, P_i, P_p
-    csc_spfree(data->A); // free A, A_x, A_i, A_p
+    csc_spfree(P);
+    csc_spfree(data->A); // free A
     csc_spfree(data->P);
     c_free(data->q); // free q
     c_free(data->l); // free l
@@ -299,13 +264,12 @@ public:
                 int verbose,
                 double* ret_status){
     // Load problem data
-    for (size_t j = 0; j < state_len; j++) {
-      size_t num=0;
-      for(size_t i = 0; i < state_len; i++){
-        if(Psparse[i*state_len+j]!=0){
-          // make P_x symmetric
-          P_x[P_p[j]+num] = (eval_weight_matrix[i*state_len+j] + eval_weight_matrix[j*state_len+i]) / 2.0;
-          num++;
+    {
+      size_t n = 0;
+      for(size_t j = 0; j < state_len; j++){
+        for(size_t i = P->p[j]; i < P->p[j+1]; i++){
+          P->x[n] = (eval_weight_matrix[P->i[i]*state_len+j]  + eval_weight_matrix[j*state_len+P->i[i]]) / 2.0;
+          n++;
         }
       }
     }
@@ -314,12 +278,12 @@ public:
       q[i] = eval_coeff_vector[i];
     }
 
-    for (size_t j = 0; j < state_len; j++) {
-      size_t num=0;
-      for(size_t i = 0; i < inequality_len; i++){
-        if(Asparse[i*state_len+j]!=0){
-          A_x[A_p[j]+num] = inequality_matrix[i*state_len+j];
-          num++;
+    {
+      size_t n = 0;
+      for(size_t j = 0; j < state_len; j++){
+        for(size_t i = A->p[j]; i < A->p[j+1]; i++){
+          A->x[n] = inequality_matrix[A->i[i]*state_len+j];
+          n++;
         }
       }
     }
@@ -330,7 +294,7 @@ public:
     }
 
     csc *P_triu = csc_to_triu(P); // P should be upper requangle
-    osqp_update_P_A(work, P_triu->x, OSQP_NULL, P_triu->p[P_triu->n] , A_x, OSQP_NULL, A_nnz);
+    osqp_update_P_A(work, P_triu->x, OSQP_NULL, P_triu->p[P_triu->n] , A->x, OSQP_NULL, A->nzmax);
     c_free(P_triu);
     if(osqp_update_lin_cost(work,q) != 0){
       *ret_status = -100.0;
@@ -359,19 +323,8 @@ private:
   size_t state_len;
   size_t inequality_len;
 
-  std::vector<double> Psparse;
-  std::vector<double> Asparse;
-
-  c_int P_nnz;
-  c_float *P_x;
-  c_int *P_i;
-  c_int *P_p;
   csc *P;
   c_float *q;
-  c_int A_nnz;
-  c_float *A_x;
-  c_int *A_i;
-  c_int *A_p;
   csc *A;
   c_float *l;
   c_float *u;
@@ -383,7 +336,7 @@ private:
 
 };
 
-std::vector<std::pair<std::pair<int, int>, std::vector<std::pair<std::pair<std::vector<double>, std::vector<double> >, boost::shared_ptr<osqp_solver> > > > > sqp_map;
+std::vector<std::pair<std::pair<csc*, csc*>, boost::shared_ptr<osqp_solver> > > sqp_map;
 
 // Solve SQP
 double* solve_osqp_hotstart (double* ret,
@@ -393,105 +346,134 @@ double* solve_osqp_hotstart (double* ret,
                              int eval_weight_matrix_sparce_given, double* eval_weight_matrix_sparce,
                              int inequality_matrix_sparce_given, double* inequality_matrix_sparce) {
   boost::shared_ptr<osqp_solver> solver;
-  std::pair<int, int> tmp_pair(state_len, inequality_len);
-  std::vector<std::pair<std::pair<int, int>, std::vector<std::pair<std::pair<std::vector<double>, std::vector<double> >, boost::shared_ptr<osqp_solver> > > > >::iterator it;
-  for(it = sqp_map.begin();it != sqp_map.end();it++){
-    if (it->first == tmp_pair) break;
-  }
-  if (it != sqp_map.end()) {
-    std::vector<std::pair<std::pair<std::vector<double>, std::vector<double> >, boost::shared_ptr<osqp_solver> > >::iterator it2;
-    for(it2 = it->second.begin();it2 != it->second.end();it2++){
-      bool match = true;
-      if(!eval_weight_matrix_sparce_given){
-        for(size_t i = 0; i < state_len; i++){
-          for(size_t j = 0; j < state_len; j++){
-            if(it2->first.first[i*state_len+j] == 0){
-              match = false;
-              break;
-            }
-          }
-          if(!match) break;
-        }
-      }else{
-        for(size_t i = 0; i < state_len; i++){
-          for(size_t j = 0; j < state_len; j++){
-            if((it2->first.first[i*state_len+j] == 0 && eval_weight_matrix_sparce[i*state_len+j] != 0) || (it2->first.first[i*state_len+j] == 0 && eval_weight_matrix_sparce[j*state_len+i] != 0) || (it2->first.first[i*state_len+j] != 0 && eval_weight_matrix_sparce[i*state_len+j] == 0 && eval_weight_matrix_sparce[j*state_len+i] == 0)){
-              match = false;
-              break;
-            }
-          }
-          if(!match) break;
-        }
-      }
-      if(match){
-        if(!inequality_matrix_sparce_given){
-          for(size_t i = 0; i < inequality_len; i++){
-            for(size_t j = 0; j < state_len; j++){
-              if(it2->first.second[i*state_len+j] == 0){
-                match = false;
-                break;
-              }
-            }
-            if(!match) break;
-          }
-        }else{
-          for(size_t i = 0; i < inequality_len; i++){
-            for(size_t j = 0; j < state_len; j++){
-              if((it2->first.second[i*state_len+j] == 0 && inequality_matrix_sparce[i*state_len+j]!=0) || (it2->first.second[i*state_len+j] != 0 && inequality_matrix_sparce[i*state_len+j]==0)){
-                match = false;
-                break;
-              }
-            }
-            if(!match) break;
-          }
-        }
-      }
-      if(match)break;
-    }
-    if (it2 != it->second.end()) solver = it2->second;
-  }
 
-  if (!solver){
-    std::vector<double> Psparse(state_len*state_len,0);
-    std::vector<double> Asparse(state_len*inequality_len,0);
-    if(!eval_weight_matrix_sparce_given){
-      for(size_t i = 0; i < state_len; i++){
-        for(size_t j = 0; j < state_len; j++){
-          Psparse[i*state_len+j] = 1.0;
-        }
+  std::vector<double> Psparse(state_len*state_len,0);
+  if(!eval_weight_matrix_sparce_given){
+    for(size_t i = 0; i < state_len; i++){
+      for(size_t j = 0; j < state_len; j++){
+        Psparse[i*state_len+j] = 1.0;
       }
-    }else{
-      for(size_t i = 0; i < state_len; i++){
+    }
+  }else{
+    for(size_t i = 0; i < state_len; i++){
         for(size_t j = 0; j < state_len; j++){
           // make Psparse symmetric
           Psparse[i*state_len+j] = (eval_weight_matrix_sparce[i*state_len+j]==0 || eval_weight_matrix_sparce[j*state_len+i]==0)? 0.0 : 1.0;
         }
+    }
+  }
+  c_int P_nnz(Psparse.size() - std::count(Psparse.begin(), Psparse.end(), 0));
+  c_float *P_x(new c_float[P_nnz]);
+  c_int *P_i(new c_int[P_nnz]);
+  c_int *P_p(new c_int[state_len+1]);
+  csc *P(csc_matrix(state_len, state_len, P_nnz, P_x, P_i, P_p));
+  P_p[0] = 0;
+  for (size_t j = 0; j < state_len; j++) {
+    size_t num=0;
+    for(size_t i = 0; i < state_len; i++){
+      if(Psparse[i*state_len+j]!=0){
+        P_i[P_p[j]+num] = i;
+        P_x[P_p[j]+num] = 0.0;
+        num++;
       }
     }
-    if(!inequality_matrix_sparce_given){
+    P_p[j+1] = P_p[j] + num;
+  }
+
+  c_int A_nnz(0);
+  for(size_t i = 0; i < inequality_len; i++){
+    for(size_t j = 0; j < state_len; j++){
+      if(inequality_matrix_sparce[i*state_len+j]!=0) A_nnz++;
+    }
+  }
+  c_float *A_x(new c_float[A_nnz]);
+  c_int *A_i(new c_int[A_nnz]);
+  c_int *A_p(new c_int[state_len+1]);
+  csc *A(csc_matrix(inequality_len, state_len, A_nnz, A_x, A_i, A_p));
+  A_p[0] = 0;
+  if(!inequality_matrix_sparce_given){
+    for (size_t j = 0; j < state_len; j++) {
+      size_t num=0;
       for(size_t i = 0; i < inequality_len; i++){
-        for(size_t j = 0; j < state_len; j++){
-          Asparse[i*state_len+j] = 1.0;
+        A_i[A_p[j]+num] = i;
+        A_x[A_p[j]+num] = 0.0;
+        num++;
+      }
+      A_p[j+1] = A_p[j] + num;
+    }
+  }else{
+    for (size_t j = 0; j < state_len; j++) {
+      size_t num=0;
+      for(size_t i = 0; i < inequality_len; i++){
+        if(inequality_matrix_sparce[i*state_len+j]!=0){
+          A_i[A_p[j]+num] = i;
+          A_x[A_p[j]+num] = 0.0;
+          num++;
         }
       }
-    }else{
-      for(size_t i = 0; i < inequality_len; i++){
-        for(size_t j = 0; j < state_len; j++){
-          Asparse[i*state_len+j] = (inequality_matrix_sparce[i*state_len+j]==0)? 0.0 : 1.0;
-        }
+      A_p[j+1] = A_p[j] + num;
+    }
+  }
+
+  std::pair<csc*, csc*> tmp_pair(P, A);
+
+  std::vector<std::pair<std::pair<csc*, csc*>, boost::shared_ptr<osqp_solver> > >::iterator it;
+  for(it = sqp_map.begin();it != sqp_map.end();it++){
+    bool match = true;
+
+    if(it->first.first->m != tmp_pair.first->m ||
+       it->first.first->n != tmp_pair.first->n ||
+       it->first.first->nzmax != tmp_pair.first->nzmax) continue;
+    if(it->first.second->m != tmp_pair.second->m ||
+       it->first.second->n != tmp_pair.second->n ||
+       it->first.second->nzmax != tmp_pair.second->nzmax) continue;
+
+    for(size_t i = 0; i < state_len+1; i++){
+      if(it->first.first->p[i] != tmp_pair.first->p[i]){
+        match = false;
+        break;
       }
     }
-    solver = boost::shared_ptr<osqp_solver>(new osqp_solver(state_len,inequality_len,Psparse,Asparse,verbose));
+    if(!match) continue;
+
+    for(size_t i=0; i < it->first.first->nzmax; i++){
+      if(it->first.first->i[i] != tmp_pair.first->i[i]){
+        match = false;
+        break;
+      }
+    }
+    if(!match) continue;
+
+    for(size_t i = 0; i < state_len+1; i++){
+      if(it->first.second->p[i] != tmp_pair.second->p[i]){
+        match = false;
+        break;
+      }
+    }
+    if(!match) continue;
+
+    for(size_t i=0; i < it->first.second->nzmax; i++){
+      if(it->first.second->i[i] != tmp_pair.second->i[i]){
+        match = false;
+        break;
+      }
+    }
+    if(!match) continue;
+
+    if(match)break;
+  }
+  if (it != sqp_map.end()) solver = it->second;
+
+  if (!solver){
+    solver = boost::shared_ptr<osqp_solver>(new osqp_solver(state_len,inequality_len,P,A,verbose));
     if(!solver->initialized){
       *ret_status = -100.0;
       return ret;
     }
-    if (it != sqp_map.end()) {
-      it->second.push_back(std::make_pair(std::make_pair(Psparse, Asparse), solver));
-    }else{
-      std::vector<std::pair<std::pair<std::vector<double>, std::vector<double> >, boost::shared_ptr<osqp_solver> > > vec(1, std::make_pair(std::make_pair(Psparse, Asparse), solver));
-      sqp_map.push_back(std::make_pair(tmp_pair, vec));
-    }
+    sqp_map.push_back(std::make_pair(tmp_pair, solver));
+  }else{
+    csc_spfree(P);
+    csc_spfree(A);
   }
 
   return solver->solve(ret,
